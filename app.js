e366ccfd110886f7de3521955e3998735fdaa768
samarbadriddin0v <info@sammi.ac>
2024-05-09 10:58:15 +0300

    #2. HTTP methods

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
+// GET, POST, DELETE, PUT
+
 const express = require('express')
 
 const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+	res.status(200).send('Hello Sammi!')
+})
+
+app.post('/', (req, res) => {
+	const { firstName, lastName } = req.body
+	const message = `His full name - ${firstName} ${lastName}`
+	res.send(message)
+})
+
+app.delete('/:id', (req, res) => {
+	const { id } = req.params
+	res.send(id)
+})
+
+app.put('/:id', (req, res) => {
+	const { id } = req.params
+	const body = req.body
+
+	res.json({ id, body })
+})
 
 const PORT = 8080
 
